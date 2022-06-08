b2634084c8c5f8f45a6113149da9ab03b27d1ef1
fhidesjoseph <fhidesjoseph@gmail.com>
2022-06-09 02:22:08 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards (name, event){
+    let  emptyArray = [];
+    for(let i=0; i<name.length; i++){
+        emptyArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+     }
+     return emptyArray;
+ }
+ let number = 10;
+ function countDown(number){
+     while(number > 0){
+         console.log(number);
+         number -=1;
+     }
+     console.log(number);
+ } 
